a1b3b7f02f8ff92c0f07b61a5a896afb700de3a4
AnkithaSAmin <124131221+AnkithaSAmin@users.noreply.github.com>
2023-03-27 12:02:29 +0530

    Add files via upload

---
+++ Modified.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+  
+
+for($x = 0; $x <= 10; $x++) {
+    if(($x%2)==0) {
+        echo $x . " is even number <br>";
+
+    } else {
+        echo $x . " is odd number <br>";
+    }
+}
+
+?>
+
+</body>
+</html>
