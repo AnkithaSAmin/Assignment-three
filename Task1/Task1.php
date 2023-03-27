55e8e381f4dae86559d5c9ebb4c1500e0cf2eac7
AnkithaSAmin <124131221+AnkithaSAmin@users.noreply.github.com>
2023-03-27 13:09:14 +0530

    Add files via upload

---
+++ Task1/Task1.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Task1</title>
+</head>
+<body>
+<?php
+
+$x=1;
+echo "Numbers are: <br>";
+ while($x<=10) {
+    echo "$x <br>";
+    $x++;
+ }
+echo "<br>";
+echo "Even Numbers are: <br>";
+for ($x=1; $x<=10; $x++)
+{
+    if(($x%2)==0)
+    {
+        echo $x. "<br>";
+    }
+}
+echo "<br>";
+echo "Odd Numbers are: <br>";
+for ($x=1; $x<=10; $x++)
+{
+    if(($x%2)!=0)
+    {
+    echo $x. "<br>";
+    }
+}
+?>
+
+</body>
+</html>
