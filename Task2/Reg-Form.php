b343b8d711a59f665a98476d55a9820481ede13f
AnkithaSAmin <124131221+AnkithaSAmin@users.noreply.github.com>
2023-03-27 14:22:37 +0530

    Add files via upload

---
+++ Task2/Reg-Form.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Task2</title>
+    </head>
+    <body>
+        <form action="form.php" method="get">
+            <h2>Registration Form</h2>
+            Name: <input type="text" name="name">
+            <br><br>
+            E-mail: <input type="text" name="email">
+            <br><br>
+            Phone-No: <input type="text" maxlength="10" name="phone">
+            <br><br>
+            Message: <textarea name="message" row="5" cols="30"></textarea>
+            <br><br>
+            Gender: 
+            <input type="radio" name="gender" value="Female">Female
+            <input type="radio" name="gender" value="Male">Male
+            <input type="radio" name="gender" value="Other">Other
+            <br><br>
+            <input type="submit" name="submit" value="Submit"> 
+        </form>
+    </body>
+</html>
+
