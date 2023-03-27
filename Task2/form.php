b28a206d8fd037ed82dce7a19227ee1a72a74e51
AnkithaSAmin <124131221+AnkithaSAmin@users.noreply.github.com>
2023-03-27 15:11:19 +0530

    Update form.php

--- Task2/form.php
+++ Task2/form.php
@@ -1,13 +1,6 @@
-<html>
-    <body>
-
-
-        <h2>Your Details</h2>
-        Your Name: <?php echo $_GET["name"]; ?><br>
-        Your Email address: <?php echo $_GET["email"]; ?><br>
-        Your Phone Number:  <?php echo $_GET["phone"]; ?><br>
-        Message:  <?php echo $_GET["message"]; ?><br>
-        Gender:  <?php echo $_GET["gender"]; ?><br>
-
-    </body>
-</html>
+<h2>Your Details</h2>
+Your Name: <?php echo $_GET["name"]; ?><br>
+Your Email address: <?php echo $_GET["email"]; ?><br>
+Your Phone Number:  <?php echo $_GET["phone"]; ?><br>
+Message:  <?php echo $_GET["message"]; ?><br>
+Gender:  <?php echo $_GET["gender"]; ?><br>
